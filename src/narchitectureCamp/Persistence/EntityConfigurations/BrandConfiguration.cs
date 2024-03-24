84d2e8e1e8870d83f024c4d1c3ac07a0d9f2541b
aysekardas <aysekardas7@gmail.com>
2024-03-24 23:10:37 +0300

    add cqrs for models

--- src/narchitectureCamp/Persistence/EntityConfigurations/BrandConfiguration.cs
+++ src/narchitectureCamp/Persistence/EntityConfigurations/BrandConfiguration.cs
@@ -15,7 +15,7 @@ public class BrandConfiguration : IEntityTypeConfiguration<Brand>
         builder.Property(b => b.CreatedDate).HasColumnName("CreatedDate").IsRequired();
         builder.Property(b => b.UpdatedDate).HasColumnName("UpdatedDate");
         builder.Property(b => b.DeletedDate).HasColumnName("DeletedDate");
-
+        builder.HasMany(b => b.Models);
         builder.HasQueryFilter(b => !b.DeletedDate.HasValue);
     }
 }
